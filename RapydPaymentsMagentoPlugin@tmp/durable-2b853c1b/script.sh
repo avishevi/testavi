e01886e9c5fa45844b36a03de7d14930b1f5e8e2
avishevi <72860598+avishevi@users.noreply.github.com>
2021-01-12 17:43:23 +0200

    Delete RapydPaymentsMagentoPlugin@tmp directory

--- RapydPaymentsMagentoPlugin@tmp/durable-2b853c1b/script.sh
+++
@@ -1,7 +0,0 @@
-
-                       pwd
-                       git add -A
-                       git config --global user.email "jenkins@jenkins.com"
-                       git commit -m "pushed from bitbucket"
-                       git push origin HEAD:main --force 
-                       
